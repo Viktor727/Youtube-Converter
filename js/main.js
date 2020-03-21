7fba15495c01d0a96bd519cb14773b61cdab2734
Viktor727 <Viktorshmatko228@gmail.com>
2020-03-21 21:42:00 +0200

    fixed css feature

--- js/main.js
+++ js/main.js
@@ -22,64 +22,73 @@
      }
  });
 
-    jQuery("body").on('click', '[href*="#"]', function (e) {
-        var fixed_offset = 20;
-        jQuery('html,body').stop().animate({
-            scrollTop: jQuery(this.hash).offset().top - fixed_offset
-        }, 1000);
-        e.preventDefault();
-    });
+ jQuery("body").on('click', '[href*="#"]', function (e) {
+     var fixed_offset = 20;
+     jQuery('html,body').stop().animate({
+         scrollTop: jQuery(this.hash).offset().top - fixed_offset
+     }, 1000);
+     e.preventDefault();
+ });
 
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var formBtn = this.document.querySelector(".form-btn");
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            formBtn.addEventListener('click', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-                let checkValidation = form.querySelectorAll('.group');
-                let arr = [];
-                for (let i = 0; i < checkValidation.length; i++) {
-                    let checkProc = checkValidation[i].innerText.replace(checkValidation[i].getElementsByTagName('label')[0].innerText + '\n', '');
-                    console.log(checkValidation[i].getElementsByTagName('label')[0].innerText);
-                    arr.push(checkProc);
-                }
-                console.log(arr);
-                if (arr.every(x => x === 'Looks good !')) {
-                    sendAjaxForm('result_form', 'contact-form', 'process.php');
-                } else {
-                    console.log('novalide');
-                }
+ (function () {
+     'use strict';
+     window.addEventListener('load', function () {
+         // Fetch all the forms we want to apply custom Bootstrap validation styles to
+         var forms = document.getElementsByClassName('needs-validation');
+         // Loop over them and prevent submission
+         var formBtn = this.document.querySelector(".form-btn");
+         var validation = Array.prototype.filter.call(forms, function (form) {
+             formBtn.addEventListener('click', function (event) {
+                 if (form.checkValidity() === false) {
+                     event.preventDefault();
+                     event.stopPropagation();
+                 }
+                 form.classList.add('was-validated');
+                 let checkValidation = form.querySelectorAll('.group');
+                 let arr = [];
+                 for (let i = 0; i < checkValidation.length; i++) {
+                     let checkProc = checkValidation[i].innerText.replace(checkValidation[i].getElementsByTagName('label')[0].innerText + '\n', '');
+                     console.log(checkValidation[i].getElementsByTagName('label')[0].innerText);
+                     arr.push(checkProc);
+                 }
+                 console.log(arr);
+                 if (arr.every(x => x === 'Looks good !')) {
+                     sendAjaxForm('result_form', 'contact-form', 'process.php');
+                 } else {
+                     console.log('novalide');
+                 }
 
-            }, false);
-        });
-    }, false);
-})();
+             }, false);
+         });
+     }, false);
+ })();
+
+ function sendAjaxForm(result_form, form, page_url) {
+     $.ajax({
+         url: page_url, //page url
+         type: "POST", //method of sending
+         dataType: "html", //format of the data
+         data: $("#" + form).serialize(), // serialize object
+         success: function (response) { //the data sending successfull
+             swal("Sent!", "Please check email!", "success");
+         },
+         error: function (response) { // Data was not sending
+             console.log("Error! Please try again.");
+         }
+     });
+ }
 
 
+// window.addEventListener('load', function () {
+//      var iframe = document.getElementsByTagName('iframe')[0];
+//      console.log(iframe);
+//      var iframeDoc = iframe.contentWindow.document;
 
-
-
-
-
-       function sendAjaxForm(result_form, form, page_url) {
-           $.ajax({
-               url: page_url, //page url
-               type: "POST", //method of sending
-               dataType: "html", //format of the data
-               data: $("#" + form).serialize(), // serialize object
-               success: function (response) { //the data sending successfull
-                   swal("Sent!", "Please check email!", "success");
-               },
-               error: function (response) { // Data was not sending
-                   console.log("Error! Please try again.");
-               }
-           });
-       }
+//      if (iframeDoc.readyState == 'complete') {
+//          iframeDoc.body.style.backgroundColor = 'green';
+//      }
+//      iframe.onload = function () {
+//          var iframeDoc = iframe.contentWindow.document;
+//          iframeDoc.body.querySelector('.rc-anchor-logo-img').style.backgroundImage = "url('../img/google_recaptcha-icon.png')";
+//      };
+//  });
