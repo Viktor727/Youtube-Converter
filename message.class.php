579dd4f581701894a27271074f47302721407017
Ihor Bandura <ihorb@ipoft.com>
2020-03-29 19:16:30 +0300

    Added Google Authorization

--- message.class.php
+++ message.class.php
@@ -3,15 +3,59 @@
 class Message {
 	public $isOk;
 	public $message;
-	public $downloadUrl;
-	public $fileName;
+	public $downloadUrl = null;
+	public $fileName = null;
+    public $isAuthRequired = False;
+    public $isAuthorized = False;
+    public $AuthUrl = "";
 
-	public function __construct($isOk, $message, $downloadUrl, $fileName)
-	{
-        $this->isOk = $isOk;
+    public static function get_error_message($message)
+    {
+        $m = new Message();
+
+        $m->isOk = false;
+        $m->message = $message;
+
+        return $m;
+    }
+
+    public static function get_auth_error_message($message, $isAuthRequired, $isAuthorized, $AuthUrl)
+    {
+        $m = new Message();
+
+        $m->isOk = false;
+        $m->message = $message;
+        $m->isAuthRequired = $isAuthRequired;
+        $m->isAuthorized = $isAuthorized;
+        $m->AuthUrl = $AuthUrl;
+
+        return $m;
+    }
+
+    public static function get_info_message($message)
+    {
+        $m = new Message();
+
+        $this->isOk = true;
+        $this->message = $message;
+
+        return $m;
+    }
+
+    public static function get_download_message($message, $downloadUrl, $fileName)
+    {
+        $m = new Message();
+
+        $this->isOk = true;
         $this->message = $message;
         $this->downloadUrl = $downloadUrl;
         $this->fileName = $fileName;
+
+        return $m;
+    }
+
+	public function __construct()
+	{
     }
 
     public function toJSON()
